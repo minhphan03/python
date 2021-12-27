990191a27a9f17f0894b8b8dfe8cf002c813dd7d
Minh <66846878+minhphan03@users.noreply.github.com>
2021-12-26 22:36:36 -0800

    clarify algorithm

--- leetcode/jump-game.py
+++ leetcode/jump-game.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def canJump(self, nums):
-        #traversing backwards
         #solution by Nick White
         lastGoodIndex = len(nums) -1
+        
+        #traversing backwards
         for i in range(len(nums)-1, -1, -1):
             if (nums[i] + i >= lastGoodIndex):
                 lastGoodIndex = i
